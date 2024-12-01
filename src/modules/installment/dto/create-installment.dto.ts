efbe1ab83dc2c3e2584edcd736db32cc3d4055f7
Jonathan Souza <souzajonathan570@gmail.com>
2024-12-01 17:57:10 -0300

    feat: add installment dto validation

--- src/modules/installment/dto/create-installment.dto.ts
+++ src/modules/installment/dto/create-installment.dto.ts
@@ -1,10 +1,43 @@
+import {
+  IsDate,
+  IsEmpty,
+  IsEnum,
+  IsNumber,
+  IsPositive,
+  IsUUID,
+  Min,
+} from 'class-validator';
 import { InstallmentStatus } from '../../../common/enums';
 
 export class CreateInstallmentDto {
+  @IsUUID('4', { message: 'O campo "debt_id" precisa ser um UUUID' })
+  @IsEmpty({ message: 'O campo "debt_id" é obrigatório' })
   debt_id: string;
+
+  @IsNumber({}, { message: 'O campo "number_installment" deve ser um número' })
+  @IsPositive({
+    message: 'O campo "number_installment" deve ser um número positivo',
+  })
+  @Min(1, {
+    message: 'O campo "number_installment" não pode ter o valor menor que 1',
+  })
   number_installment: number;
+
+  @IsNumber({}, { message: 'O campo "amount_installment" deve ser um número' })
+  @IsPositive({
+    message: 'O campo "amount_installment" deve ser um número positivo',
+  })
+  @Min(1, {
+    message: 'O campo "amount_installment" não pode ter o valor menor que 1',
+  })
   amount_installment: number;
+
+  @IsDate({ message: 'O campo "due_date" deve ser uma data válida' })
   due_date: Date;
+
+  @IsDate({ message: 'O campo "payment_date" deve ser uma data válida' })
   payment_date: Date;
+
+  @IsEnum(InstallmentStatus, { message: 'O campo "status" está invalido' })
   status: InstallmentStatus;
 }
--- src/modules/installment/dto/find-params-installment.dto.ts
+++ src/modules/installment/dto/find-params-installment.dto.ts
@@ -1,4 +1,13 @@
+import { IsNumber, IsPositive, Min } from 'class-validator';
+
 export class FindParamsInstallmentDTO {
-  page: number;
+  @IsNumber({}, { message: 'O campo "limit" é obrigatório' })
+  @IsPositive({ message: 'O campo "limit" deve ser um numero positivo' })
+  @Min(1, { message: 'O campo "limit" não pode ser menos que 1' })
   limit: number;
+
+  @IsNumber({}, { message: 'O campo "page" é obrigatório' })
+  @IsPositive({ message: 'O campo "page" deve ser um numero positivo' })
+  @Min(1, { message: 'O campo "page" não pode ser menos que 1' })
+  page: number;
 }
