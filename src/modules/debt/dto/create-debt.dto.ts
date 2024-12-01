798690c931d91648356d84cfbdfc086720bf356e
Jonathan Souza <souzajonathan570@gmail.com>
2024-12-01 17:44:45 -0300

    feat: add dbt validations

--- src/modules/debt/dto/create-debt.dto.ts
+++ src/modules/debt/dto/create-debt.dto.ts
@@ -1,7 +1,31 @@
+import {
+  IsDate,
+  IsEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+  MinLength,
+} from 'class-validator';
+
 export class CreateDebtDTO {
+  @IsString({ message: 'O nome deve ser uma "string"' })
+  @IsEmpty({ message: 'O campo "name" é obrigatório' })
+  @MinLength(3, { message: 'O campo "name" precisa ter mais que 3 caracteres' })
   name: string;
+
+  @IsUUID('4', { message: 'O campo "category_id" precisa ser um UUUID' })
+  @IsEmpty({ message: 'O campo "category_id" é obrigatório' })
   category_id: string;
+
+  @IsNumber({}, { message: 'O campo "total_amount" deve ser um número' })
+  @IsPositive({ message: 'O campo "total_amount" deve ser um número positivo' })
   total_amount: number;
+
+  @IsNumber({}, { message: 'O campo "amount_paid" deve ser um número' })
+  @IsPositive({ message: 'O campo "amount_paid" deve ser um número positivo' })
   amount_paid: number;
+
+  @IsDate({ message: 'O campo "due_date" deve ser uma data válida' })
   due_date: Date;
 }
--- src/modules/debt/dto/find-params.dto.ts
+++ src/modules/debt/dto/find-params.dto.ts
@@ -1,13 +1,13 @@
-// import { IsNumber, IsPositive, Min } from 'class-validator';
+import { IsNumber, IsPositive, Min } from 'class-validator';
 
 export class FindParamsDTO {
-  // @IsNumber()
-  // @IsPositive({ message: '"limit" deve ser um numero positivo' })
-  // @Min(1, { message: '"limit" não pode ser menos que 1' })
+  @IsNumber({}, { message: 'O campo "limit" é obrigatório' })
+  @IsPositive({ message: 'O campo "limit" deve ser um numero positivo' })
+  @Min(1, { message: 'O campo "limit" não pode ser menos que 1' })
   limit: number;
 
-  // @IsNumber()
-  // @IsPositive({ message: '"page" deve ser um numero positivo' })
-  // @Min(1, { message: '"page" não pode ser menos que 1' })
+  @IsNumber({}, { message: 'O campo "page" é obrigatório' })
+  @IsPositive({ message: 'O campo "page" deve ser um numero positivo' })
+  @Min(1, { message: 'O campo "page" não pode ser menos que 1' })
   page: number;
 }
